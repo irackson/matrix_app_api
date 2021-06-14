cdd454a2665018da57738d738d5dc0b16c222d24
irackson <nasirus@protonmail.com>
2021-06-13 20:06:26 -0400

    Commit 10: Active Record queries - see img: https://i.imgur.com/ltBD0Oh.png

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
                    { name: 'Neo', description: 'a cybercriminal and computer programmer' },
-                   { name: 'Trinity', description: 'a truly inspirational leader and influential teacher to many people' },
-                   { name: 'Morpheus', description: 'a computer programmer and a hacker who has escaped from the Matrix' },
+                   { name: 'Morpheus', description: 'a truly inspirational leader and influential teacher to many people' },
+                   { name: 'Trinity', description: 'a computer programmer and a hacker who has escaped from the Matrix' },
                    { name: 'Agent Smith', description: 'began as an Agent, an AI program in the Matrix programmed to keep order within the system by terminating human simulacra which would bring instability to the simulated reality' }
                  ])
